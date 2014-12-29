35185bdf0a63d56ab41af1416333ce56d4a4d891
Sugihara Masaki <sugihara0227@gmail.com>
2014-12-29 17:30:32 +0900

    model,schema修正

--- app/models/repository_snapshot.rb
+++ app/models/repository_snapshot.rb
@@ -1,2 +1,7 @@
 class RepositorySnapshot < ActiveRecord::Base
+  validates :name, presence: true
+  validates :html_url, presence: true
+  validates :stargazers_count, presence: true
+  validates :language, presence: true
+  validates :create_date, presence: true
 end
--- db/migrate/20150104192559_create_repository_snapshots.rb
+++ db/migrate/20150104192559_create_repository_snapshots.rb
@@ -1,13 +1,13 @@
 class CreateRepositorySnapshots < ActiveRecord::Migration
   def change
     create_table :repository_snapshots do |t|
-      t.string :name
-      t.string :html_url
-      t.integer :stargazers_count
-      t.string :language
-      t.date :create_date
-
-      t.timestamps null: false
+      t.string :name, null: false
+      t.string :html_url, null: false
+      t.integer :stargazers_count, null: false
+      t.string :language, null: false
+      t.date :create_date, null: false
     end
+    add_index :repository_snapshots, [:create_date, :language]
+    add_index :repository_snapshots, :language
   end
 end
