0e5cb43d840ed0b5fab8f6761acf4e13133636a9
Sugihara Masaki <sugihara0227@gmail.com>
2014-12-31 06:47:31 +0900

    scope追加、controller修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,17 @@
 class HomeController < ApplicationController
+  def index
+    @repositories =
+      if search_params[:term].present?
+        RepositorySnapshot.with_term(search_params[:term])
+      else
+        RepositorySnapshot.default_conditions
+      end
+    @repositories = @repositories.with_language(search_params[:language]) if search_params[:language].present?
+  end
+
+  private
+
+  def search_params
+    @search_params ||= params[:search].present? ? params[:search].dup : {}
+  end
 end
--- app/models/repository_snapshot.rb
+++ app/models/repository_snapshot.rb
@@ -4,4 +4,8 @@ class RepositorySnapshot < ActiveRecord::Base
   validates :stargazers_count, presence: true
   validates :language, presence: true
   validates :create_date, presence: true
+
+  scope :with_term, -> (term) { where('create_date > ?', Date.today - 1.send(term.to_sym)) }
+  scope :with_language, -> (language) { where(language: language) }
+  scope :default_conditions, -> { where('create_date > ?', Date.today - 1.week) }
 end
