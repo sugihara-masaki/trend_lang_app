c4457f1c260344194cc5c84d16720f46a1ea2f53
Sugihara Masaki <sugihara0227@gmail.com>
2014-12-30 11:26:10 +0900

    rubocop -a での修正

--- spec/factories/repository_snapshots.rb
+++ spec/factories/repository_snapshots.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :repository_snapshot do
-    name "MyString"
-    html_url "MyString"
+    name 'MyString'
+    html_url 'MyString'
     stargazers_count 1
-    language "MyString"
-    create_date "2015-01-04"
+    language 'MyString'
+    create_date '2015-01-04'
   end
 end
